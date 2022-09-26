35518ff2813f44dd98e8781479ced6a3e70346c5
Talha <talhapatel5412@gmail.com>
2022-09-26 12:04:46 +0200

    added nextjs portfolio

---
+++ personal_portfolio/components/ContactMe.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Props = {};
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+const ContactMe = (props: Props) => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:talhapatel5412@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.message}. ${formData.message}`;
+  };
+
+  return (
+    <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I have got just what you need{" "}
+          <span className="decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
+        </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+27794518891</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">talhapatel5412@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">123 developer lane</p>
+          </div>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              className="contactInput"
+              type="text"
+              name=""
+              id=""
+              placeholder="Name"
+            />
+            <input
+              {...register("email")}
+              className="contactInput"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+
+          <input
+            {...register("subject")}
+            className="contactInput"
+            type="text"
+            placeholder="Subject"
+          />
+
+          <textarea
+            {...register("message")}
+            className="contactInput"
+            placeholder="Message"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
--- personal_portfolio/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Banner } from "./Banner";
-export { default as Frameworks } from "./Frameworks";
-export { default as Contact } from "./Contact";
-export { default as Development } from "./development/Development";
-export { default as Services } from "./Services";
